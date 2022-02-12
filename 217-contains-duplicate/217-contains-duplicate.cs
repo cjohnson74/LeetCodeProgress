18d3407c4c11aee0a43d4a9241e9c8f0b0a136ff
Carson Johnson <cjohnson74.tech@gmail.com>
2022-02-11 23:52:35 -0500

    Time: 290 ms (46.78%), Space: 45.3 MB (92.97%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cs
+++ 217-contains-duplicate/217-contains-duplicate.cs
@@ -1,10 +1,10 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.Length - 1; i++)
+        Array.Sort(nums);
+        for (int i = 0 ; i < nums.Length-1; i++) 
         {
-            for (int j = nums.Length - 1; j > i; j--)
+            if (nums[i] == nums[i+1])
             {
-            if (nums[i] == nums[j])
                 return true;
             }
         }
