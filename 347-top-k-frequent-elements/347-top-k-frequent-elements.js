b996c0f8017d77bb5350698f42c63f9e9fa3e136
Carson Johnson <cjohnson74.tech@gmail.com>
2022-04-20 17:58:05 -0400

    Time: 96 ms (64.71%), Space: 45.8 MB (48.03%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -4,14 +4,21 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-  const map = new Map(); //! map to count the frequency of the number
-  for (let num of nums) {
-    map.set(num, map.get(num) + 1 || 1);
-  }
-  const result = [];
-  for (let [key, value] of map) {
-    result.push([key, value]); //! add the number and its frequency
-  }
-  result.sort((a, b) => b[1] - a[1]); //! solve with respect to the frequency of the number
-  return result.slice(0, k).map((x) => x[0]); //! slice the list with respect to length of k
+    var map = {};
+    for(let i = 0; i < nums.length; i++) {
+        if (map.hasOwnProperty(nums[i])) {
+            map[nums[i]]++;
+        } else {
+            map[nums[i]] = 1;
+        }
+    }
+    console.log(map);
+    console.log(Object.keys(map));
+    
+    var answer = [];
+    for (let key in map) {
+        answer.push([key, map[key]]);
+    }
+    answer.sort((a, b) => b[1] - a[1]);
+    return answer.slice(0, k).map((x) => x[0]);
 };
