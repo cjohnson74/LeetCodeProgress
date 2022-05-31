59bc2483fb20267b19bfbca60d3d564c6cb450a1
Carson Johnson <cjohnson74.tech@gmail.com>
2022-05-31 17:19:20 -0400

    Time: 137 ms, Memory: 44.6 MB - LeetHub

---
+++ find-pivot-index/find-pivot-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    var totalSum = 0;
+    var n = nums.length;
+    var leftSum = 0;
+    if(nums.length === 0) return -1;
+    for(var num of nums){
+        totalSum += num;
+    }
+    for(var i = 0; i < n; i++){
+        if(leftSum == (totalSum - nums[i])) {
+            return i;
+        } else {
+            leftSum += nums[i];
+            totalSum -= nums[i];
+        }
+        
+    }
+    return -1;
+};
