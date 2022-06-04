792ca25580b83005c719013d4437024634889d4c
Carson Johnson <cjohnson74.tech@gmail.com>
2022-06-04 08:46:33 -0400

    Time: 106 ms (11.86%), Space: 41.7 MB (94.79%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -7,27 +7,29 @@ var searchInsert = function(nums, target) {
     var leftIdx = 0;
     var rightIdx = nums.length - 1;
     var midIdx;
-    if(nums[rightIdx] < target) {
-        return rightIdx + 1;;
-    }
-    if(nums[leftIdx] > target) {
-        return leftIdx;
-    }
-    while(leftIdx <= rightIdx){
+    while(rightIdx - leftIdx >= 0){
         var midIdx = Math.floor((leftIdx + rightIdx) / 2);
         var midNum = nums[midIdx];
         
-        if(midNum == target){
-            return midIdx;
-        } else if (midNum > target && nums[midIdx-1] < target) {
+        if(midNum > target){
+            rightIdx = midIdx - 1;
+        } else if(midNum < target){
+            leftIdx = midIdx + 1;
+        } else {
             return midIdx;
         }
         
-        if(midNum < target){
-            leftIdx = midIdx + 1;
-        } else {
-            rightIdx = midIdx - 1;
-        }
+//         if(midNum == target){
+//             return midIdx;
+//         } else if (midNum > target && nums[midIdx-1] < target) {
+//             return midIdx;
+//         }
+        
+//         if(midNum < target){
+//             leftIdx = midIdx + 1;
+//         } else {
+//             rightIdx = midIdx - 1;
+//         }
     }
-    return midIdx;
+    return leftIdx;
 };
