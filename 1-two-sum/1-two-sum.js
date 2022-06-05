3110d7d1a02bf533b94fd4842ab9494718ec7ba6
Carson Johnson <cjohnson74.tech@gmail.com>
2022-06-05 14:27:39 -0400

    Time: 102 ms (60.73%), Space: 43 MB (35.50%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,13 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = {}
+    const map = new Map();
     
-    for(let i = 0; i < nums.length; i++){
-        var goal = target - nums[i];
-        if(map.hasOwnProperty(nums[i])){
-            return ([nums.indexOf(goal), i])
+    for(var i = 0; i < nums.length; i++) {
+        var possMatch = target - nums[i];
+        if(map.hasOwnProperty(nums[i])) {
+            return [ map[nums[i]] , i];
         }
-        map[goal] = true;
+        map[possMatch] = i;
     }
+    return [];
 };
