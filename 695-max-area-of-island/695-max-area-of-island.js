f0a38f6d6614490579725fc9f901d2bf35d78cc4
Carson Johnson <cjohnson74.tech@gmail.com>
2022-08-13 17:11:09 -0400

    Time: 186 ms (11.57%), Space: 47.2 MB (33.40%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.js
+++ 695-max-area-of-island/695-max-area-of-island.js
@@ -3,31 +3,44 @@
  * @return {number}
  */
 var maxAreaOfIsland = function(grid) {
-    const explore = (grid, row, col) => {
-    var rowInBounds = 0 <= row && grid.length > row;
-    var colInBounds = 0 <= col && grid[0].length > col;
-    if(!rowInBounds || !colInBounds) return false;
-  
-    if(grid[row][col] == 0) return false;
-    grid[row][col] = 0;
-    currentCount += 1;
-  
-    explore(grid, row-1, col, currentCount);
-    explore(grid, row+1, col, currentCount);
-    explore(grid, row, col+1, currentCount);
-    explore(grid, row, col-1, currentCount);
-    return currentCount;
-  }
-  var maximumCount = Number.MIN_VALUE;
-  var currentCount = 0;
-  for(var row = 0; row < grid.length; row++){
-    for(var col = 0; col < grid[0].length; col++){
-      currentCount = 0;
-      currentCount = explore(grid, row, col, currentCount);
-      if(currentCount != false){
-        maximumCount = Math.max(currentCount, maximumCount);
-      }
+    var rowMax = grid.length;
+    var colMax = grid[0].length;
+    var largestIsland = -Infinity;
+    var visited = new Set();
+    
+    
+    for (let row = 0; row < rowMax; row++) {
+        for (let col = 0; col < colMax; col++) {
+            var currIsland = 0;
+            if (grid[row][col] === 1) {
+                currIsland += dfs(grid, row, col, rowMax, colMax, visited);
+            }
+            largestIsland = Math.max(largestIsland, currIsland);
+        }
     }
-  }
-  return maximumCount;
+    
+    return largestIsland;
 };
+
+var dfs = function(grid, row, col, rowMax, colMax, visited) {
+    var rowInbound = row >= 0 && row < rowMax;
+    var colInbound = col >= 0 && col < colMax;
+    var inbounds = rowInbound && colInbound;
+    if (!inbounds) return 0;
+    
+    if (grid[row][col] === 0) return 0;
+    
+    var pos = row + ',' + col;
+    if (visited.has(pos)) {
+        return 0;
+    } else {
+        visited.add(pos);
+    }
+    
+    var size = 1;
+    size += dfs(grid, row-1, col, rowMax, colMax, visited);
+    size += dfs(grid, row+1, col, rowMax, colMax, visited);
+    size += dfs(grid, row, col-1, rowMax, colMax, visited);
+    size += dfs(grid, row, col+1, rowMax, colMax, visited);
+    return size;
+}
