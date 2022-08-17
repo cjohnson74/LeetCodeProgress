70c126fd402298370472b80558303df18182c7f1
Carson Johnson <cjohnson74.tech@gmail.com>
2022-08-17 16:45:43 -0400

    Time: 162 ms (74.59%), Space: 57.5 MB (70.38%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    if (nums == null || nums.length === 0) return 0;
-  
-  const set = new Set(nums);
-  let max = 0;
-
-  for (let num of set) {
-    if (set.has(num - 1)) continue;  // make sure starting from the beginning of sequence
-
-    let currNum = num;
-    let currMax = 1;
-
-    while (set.has(currNum + 1)) {
-      currNum++;
-      currMax++;
+    if (nums.length == 0 || nums == null) return 0;
+    
+    var set = new Set(nums);
+    var longest = -Infinity;
+    
+    for (let num of set) {
+        if (set.has(num-1)) continue;
+        
+        let currNum = num;
+        let currStreak = 1;
+        
+        while(set.has(currNum+1)) {
+            currNum++;
+            currStreak++;
+        }
+        longest = Math.max(currStreak, longest);
     }
-    max = Math.max(max, currMax);
-  }
-
-  return max;
+    
+    return longest;
 };
