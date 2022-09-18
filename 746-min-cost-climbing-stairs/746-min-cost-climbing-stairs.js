d55efd32ca53f9dc7678749f2c0336c017d76efb
Carson Johnson <cjohnson74.tech@gmail.com>
2022-09-18 09:54:02 -0400

    Time: 119 ms (19.94%), Space: 44.1 MB (40.24%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+    cost.push(0);
+    
+    for (let i = cost.length-3; i >= 0; i--) {
+        cost[i] += Math.min(cost[i + 1], cost[i + 2]);
+    }
+    
+    return Math.min(cost[0], cost[1]);
+};
