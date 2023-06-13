edccf9a7b8f8df78364075c1093fd99c949329de
Carson Johnson <cjohnson74.tech@gmail.com>
2023-06-12 22:06:57 -0700

    Time: 85 ms (27.70%), Space: 16.3 MB (51.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,50 +12,37 @@ class Solution:
         while currNode:
             num1.append(currNode.val)
             currNode = currNode.next
-        print(num1)
             
         currNode = l2
         while currNode:
             num2.append(currNode.val)
             currNode = currNode.next
-        print(num2)
         
         if len(num1) > 0:
             num1.reverse()
             num1str = ''.join([str(num) for num in num1])
         else:
             num1 = '0'
-        print(num1str)
         
-        print(num1)
         if len(num2) > 0:
             num2.reverse()
             num2str = ''.join([str(num) for num in num2])
         else:
             num2 = '0'
-        print(num2str)
         
         sumNums = int(num1str) + int(num2str)
-        print(sumNums)
         
         sumArray = []
         for num in str(sumNums):
             sumArray.append(num)
         sumArray.reverse()
-        print(sumArray)
         
         dummy = ListNode(0)
         sumNode = ListNode(0)
         dummy = sumNode
         
         for num in sumArray:
-            print(num)
             sumNode.next = ListNode(num)
             sumNode = sumNode.next
-
-        currNode = dummy.next
-        while currNode:
-            print(currNode)
-            currNode = currNode.next
         
         return dummy.next
