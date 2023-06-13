06c17a16e008ce1071b83872b3e06a279d280830
Carson Johnson <cjohnson74.tech@gmail.com>
2023-06-12 23:16:18 -0700

    Time: 86 ms (23.48%), Space: 16.3 MB (51.63%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,44 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        currNode = l1
-        num1 = []
-        num2 = []
+        dummy = ListNode()
+        curr = dummy
         
-        while currNode:
-            num1.append(currNode.val)
-            currNode = currNode.next
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
             
-        currNode = l2
-        while currNode:
-            num2.append(currNode.val)
-            currNode = currNode.next
-        
-        if len(num1) > 0:
-            num1.reverse()
-            num1str = ''.join([str(num) for num in num1])
-        else:
-            num1 = '0'
-        
-        if len(num2) > 0:
-            num2.reverse()
-            num2str = ''.join([str(num) for num in num2])
-        else:
-            num2 = '0'
-        
-        sumNums = int(num1str) + int(num2str)
-        
-        sumArray = []
-        for num in str(sumNums):
-            sumArray.append(num)
-        sumArray.reverse()
-        
-        dummy = ListNode(0)
-        sumNode = ListNode(0)
-        dummy = sumNode
-        
-        for num in sumArray:
-            sumNode.next = ListNode(num)
-            sumNode = sumNode.next
-        
+            val = v1 + v2 + carry
+            
+            carry = val // 10
+            val = val % 10
+            
+            curr.next = ListNode(val)
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            curr = curr.next
+            
         return dummy.next
