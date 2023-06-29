413f7608dcb35493b9bafb5fb7f1a893d64e7ecf
Carson Johnson <cjohnson74.tech@gmail.com>
2023-06-28 19:39:11 -0700

    Time: 416 ms (22.57%), Space: 24.4 MB (19.38%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,7 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        res = 0
+        islands = 0
         rows, cols = len(grid), len(grid[0])
         visited = set()
         
@@ -15,20 +15,20 @@ class Solution:
                 currRow, currCol = queue.popleft()
                 
                 deltas = [(0,1),(0,-1),(1,0),(-1,0)]
-                for delta in deltas:
-                    deltaRow, deltaCol = delta
-                    rowNeighbor = currRow + deltaRow
-                    colNeighbor = currCol + deltaCol
-                    if rowNeighbor >= 0 and rowNeighbor < len(grid) and colNeighbor >= 0 and colNeighbor < len(grid[0]) and (rowNeighbor, colNeighbor) not in visited:
-                        if grid[rowNeighbor][colNeighbor] == '1':
-                            queue.append((rowNeighbor, colNeighbor))
-                            visited.add((rowNeighbor, colNeighbor))
+                for deltaRow, deltaCol in deltas:
+                    rowNeighbor, colNeighbor = currRow + deltaRow, currCol + deltaCol
+                    if (rowNeighbor in range(rows) and 
+                        colNeighbor in range(cols) and
+                        grid[rowNeighbor][colNeighbor] == '1' and
+                       (rowNeighbor, colNeighbor) not in visited):
+                        queue.append((rowNeighbor, colNeighbor))
+                        visited.add((rowNeighbor, colNeighbor))
         
         
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 if grid[row][col] == '1' and (row, col) not in visited:
                     bfs(row, col)
-                    res += 1
+                    islands += 1
                     
-        return res
+        return islands
