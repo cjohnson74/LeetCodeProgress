24cd051b4f9d8c00864a79a59f69b8aa1788e615
Carson Johnson <cjohnson74.tech@gmail.com>
2023-06-28 19:32:40 -0700

    Time: 357 ms (45.03%), Space: 24.3 MB (22.96%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
         res = 0
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
         
         def bfs(row, col):
-            queue = [[row, col]]
+            queue = collections.deque()
+            visited.add((row, col))
+            queue.append((row, col))
             
             while queue:
-                [currRow, currCol] = queue.pop(0)
+                currRow, currCol = queue.popleft()
                 
-                deltas = [[0,1],[0,-1],[1,0],[-1,0]]
+                deltas = [(0,1),(0,-1),(1,0),(-1,0)]
                 for delta in deltas:
-                    [deltaRow, deltaCol] = delta
+                    deltaRow, deltaCol = delta
                     rowNeighbor = currRow + deltaRow
                     colNeighbor = currCol + deltaCol
-                    if rowNeighbor >= 0 and rowNeighbor < len(grid) and colNeighbor >= 0 and colNeighbor < len(grid[0]):
+                    if rowNeighbor >= 0 and rowNeighbor < len(grid) and colNeighbor >= 0 and colNeighbor < len(grid[0]) and (rowNeighbor, colNeighbor) not in visited:
                         if grid[rowNeighbor][colNeighbor] == '1':
-                            queue.append([rowNeighbor, colNeighbor])
-                            grid[rowNeighbor][colNeighbor] = '0'
+                            queue.append((rowNeighbor, colNeighbor))
+                            visited.add((rowNeighbor, colNeighbor))
         
         
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == '1':
-                    grid[row][col] = '0'
+                if grid[row][col] == '1' and (row, col) not in visited:
                     bfs(row, col)
                     res += 1
                     
