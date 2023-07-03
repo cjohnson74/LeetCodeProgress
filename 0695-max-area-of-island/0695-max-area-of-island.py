0bd6f411d523ae5fb21ba48cadb9a0ab97e999d7
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-03 07:29:50 -0700

    Time: 133 ms (94.80%), Space: 16.4 MB (99.76%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,28 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
         maxArea = 0
         
         def bfs(row, col):
             stack = collections.deque()
             stack.append((row, col))
-            area = 0
+            area = 1
             
             while stack:
                 row, col = stack.pop()
                 grid[row][col] = 0
-                area += 1
                 
                 deltas = [(0,1),(0,-1),(1,0),(-1,0)]
                 for deltaRow, deltaCol in deltas:
                     neighborRow, neighborCol = row + deltaRow, col + deltaCol
-                    if neighborRow >= 0 and neighborRow < len(grid) and neighborCol >= 0 and neighborCol < len(grid[0]) and grid[neighborRow][neighborCol] == 1:
-                        area += bfs(neighborRow, neighborCol)
+                    if neighborRow >= 0 and neighborRow < ROWS and neighborCol >= 0 and neighborCol < COLS and grid[neighborRow][neighborCol] == 1:
+                        stack.append((neighborRow, neighborCol))
+                        grid[neighborRow][neighborCol] = 0
+                        area += 1
                     
             return area
             
             
-        for row in range(len(grid)):
-            for col in range(len(grid[row])):
+        for row in range(ROWS):
+            for col in range(COLS):
                 if grid[row][col] == 1:
                     maxArea = max(bfs(row, col), maxArea)
                     
