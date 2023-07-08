5a2c1eabaac0ef640dd7c303669607c7097734f8
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-08 11:18:23 -0700

    Time: 1081 ms (17.34%), Space: 27.2 MB (94.15%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # left and right pointer
+        # keep track of max profit seen
+        # increment right pointer until prices[right] < prices[left]
+            # then make left pointer = right pointer
+        # while right pointer < len(prices)
+        # return maxProfit
+        
+        maxProfit = 0
+        left, right = 0, 0
+        
+        while right < len(prices):
+            profit = prices[right] - prices[left]
+            maxProfit = max(maxProfit, profit)
+            
+            if prices[right] < prices[left]:
+                left = right
+            else:
+                right += 1
+                
+        return maxProfit
