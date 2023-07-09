ea373474892e3055a2d7781ab7407fa959118228
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-09 11:40:50 -0700

    Time: 73 ms (60.51%), Space: 16.4 MB (91.41%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -6,25 +6,32 @@ class Solution:
         # at each step return min(dfs(step+1), dfs(step+2))
         
         # return min(dfs(step+1), dfs(step+2))
-        # time: n^2 because we are making two decisions at every step and repeated work
+        # time: 2^n because we are making two decisions at every step and repeated work
         # space: n being the deepest branch in the decision tree
         # reduce time: O(n) using dp/memoization
         # space: n being the number of steps because we cut out repeated work with memo
         # time: O(n)
         # reduce space: O(1)
         
-        def dfs(step, memo = {}):
-            if step in memo:
-                return memo[step]
-            
-            if step == len(cost)-1:
-                return cost[step]
-            
-            if step > len(cost) - 1:
-                return 0
-            
-            minCost = cost[step] + min(dfs(step + 1), dfs(step + 2))
-            memo[step] = minCost
-            return minCost
+        cost.append(0)
         
-        return min(dfs(0), dfs(1))
+        for step in range(len(cost)-3, -1, -1):
+            cost[step] = cost[step] + min(cost[step + 1], cost[step + 2])
+            
+        return min(cost[0], cost[1])
+        
+#         def dfs(step, memo = {}):
+#             if step in memo:
+#                 return memo[step]
+            
+#             if step == len(cost)-1:
+#                 return cost[step]
+            
+#             if step > len(cost) - 1:
+#                 return 0
+            
+#             minCost = cost[step] + min(dfs(step + 1), dfs(step + 2))
+#             memo[step] = minCost
+#             return minCost
+        
+#         return min(dfs(0), dfs(1))
