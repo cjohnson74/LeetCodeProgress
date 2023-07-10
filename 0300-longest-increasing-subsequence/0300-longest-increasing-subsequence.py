711194a3e6f59e469abe544db3c51217ce9c8a8a
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-09 21:41:59 -0700

    Time: 3431 ms (47.11%), Space: 16.7 MB (72.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -15,22 +15,30 @@ class Solution:
         # time: n with memoization
         # space: n length of nums
         
-        def dfs(i, memo = {}):
-            if i in memo:
-                return memo[i]
-            if i == len(nums):
-                return 0
-            
-            currMaxLen = 1
+        dp = [1] * len(nums)
+        
+        for i in range(len(nums) -1, -1, -1):
             for j in range(i+1, len(nums)):
                 if nums[i] < nums[j]:
-                    currMaxLen = max(currMaxLen, dfs(j) + 1)
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+        
+#         def dfs(i, memo = {}):
+#             if i in memo:
+#                 return memo[i]
+#             if i == len(nums):
+#                 return 0
             
-            memo[i] = currMaxLen
-            return currMaxLen
+#             currMaxLen = 1
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] < nums[j]:
+#                     currMaxLen = max(currMaxLen, dfs(j) + 1)
+            
+#             memo[i] = currMaxLen
+#             return currMaxLen
         
-        maxLength = 0
-        for i in range(len(nums)):
-            maxLength = max(maxLength, dfs(i))
+#         maxLength = 0
+#         for i in range(len(nums)):
+#             maxLength = max(maxLength, dfs(i))
         
-        return maxLength
+#         return maxLength
