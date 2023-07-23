aa3554509fdfc90b60edc02cc591ab502b5c3fa4
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-23 07:11:05 -0700

    Time: 719 ms (55.75%), Space: 16.4 MB (47.59%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # loop through array
-        # have pointers starting at letter
-        # pointers go out while they are in bounds and while the string is a pali
-        # keep track of result
-        # check len of currString in while loop and update result
-        # time: O(n^2) n being the number of letters in the string
-        # space: O(n) n being the length of our result/inputstring
-        
-        result = ''
-        greatestLength = 0
-        
+        longestSubstringLeft, longestSubstringRight = 0, 0
+        longestSubstringLen = 0
         for i in range(len(s)):
-            # check odds
             left, right = i, i
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 currLength = right - left + 1
-                if currLength > greatestLength:
-                    result = s[left:right+1]
-                    greatestLength = currLength
+                if longestSubstringLen < currLength:
+                    longestSubstringLeft, longestSubstringRight = left, right + 1
+                    longestSubstringLen = currLength
                 left -= 1
                 right += 1
-                
-            # check evens
-            left, right = i, i+1
+            
+            left, right = i, i + 1
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                currLength = (right - left + 1)
-                if currLength > greatestLength:
-                    result = s[left:right+1]
-                    greatestLength = currLength
+                currLength = right - left + 1
+                if longestSubstringLen < currLength:
+                    longestSubstringLeft, longestSubstringRight = left, right + 1
+                    longestSubstringLen = currLength
                 left -= 1
                 right += 1
-                
-        return result
+                    
+        return s[longestSubstringLeft:longestSubstringRight]
+            
