ca56c6eee4acb1aeaaf95d7123306135a9e82180
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-23 06:46:08 -0700

    Time: 45 ms (75.76%), Space: 16.3 MB (45.81%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,15 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
         
-        def robHelper(startHouse, endHouse):
-            rob1, rob2 = 0, 0
-            
-            for house in range(startHouse, endHouse):
-                newRob = max(nums[house] + rob1, rob2)
-                rob1 = rob2
-                rob2 = newRob
-            
-            return rob2
         
-        # nums[0] is an edge case that is if len(nums) == 1 then that would be the max returned
-        return max(nums[0], robHelper(0, len(nums)-1), robHelper(1, len(nums)))
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+        
+        for num in nums:
+            temp = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = temp
+            
+        return max(rob1, rob2)
