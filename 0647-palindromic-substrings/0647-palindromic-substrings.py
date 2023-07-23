515f395ffe46aee3dbe2ed8333d484c95b28a7ab
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-23 07:14:03 -0700

    Time: 148 ms (46.18%), Space: 16.4 MB (54.07%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,27 +1,18 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        # loop through s
-        # check for palidromes by have a left and right pointer starting at middle and expanding
-        # check odd by starting at left, right = i, i
-        # check even by starting at left, right = i, i+1
-        # in while loops increase the number of palidromes by 1
-        
-        numOfPalindromes = 0
+        numOfSubstrings = 0
         
         for i in range(len(s)):
-            # check odd pali
-            numOfPalindromes += self.numPali(s, i, i)
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                numOfSubstrings += 1
+                left -= 1
+                right += 1
                 
-            # check even pali
-            numOfPalindromes += self.numPali(s, i, i+1)
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                numOfSubstrings += 1
+                left -= 1
+                right += 1
                 
-        return numOfPalindromes
-    
-    def numPali(self, s, left, right):
-        result = 0
-        while left >= 0 and right < len(s) and s[left] == s[right]:
-            result += 1
-            left -= 1
-            right += 1
-            
-        return result
+        return numOfSubstrings
