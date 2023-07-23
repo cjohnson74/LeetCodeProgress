ffc07c6bd475dc0ace8113a878326d8ea59e3a00
Carson Johnson <cjohnson74.tech@gmail.com>
2023-07-23 11:17:06 -0700

    Time: 40 ms (93.60%), Space: 16.4 MB (33.12%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,26 +1,17 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # dfs
-        # either move once or move twice since integer length cannot be more than 2
-        # if index == len(s) - 1: return 1 if currInt in intToChar map else 0
-        # if currInt < 0 or currInt >= 27: return 0
-        # return dfs(index+1, s[index:index+2]) + dfs(index+2, s[index:index+3])
-        dp = { len(s): 1 }
+        one, two = 1, 1
         
-        def dfs(index):
-            if index in dp:
-                return dp[index]
-
-            if s[index] == '0': 
-                return 0
+        for i in range(len(s)-1, -1, -1):
+            temp = 0
+            if s[i] != '0':
+                temp = one
+                
+            if (i + 1 < len(s) and (s[i] == '1' or s[i] == '2' and s[i + 1] in '0123456')):
+                temp += two
+                
+            two = one
+            one = temp
             
-            res = dfs(index + 1)
-            
-            if (index + 1 < len(s) and (s[index] == '1' or s[index] == '2' and s[index + 1] in '0123456')):
-                res += dfs(index + 2)
-            dp[index] = res
-            return res
-        
-        return dfs(0)
-                                                 
-        
+        return one
+                
