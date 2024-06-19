ce86ed32e39e4fb17eb0b317de36b8ba042d0bd4
Carson Johnson <cjohnson74.tech@gmail.com>
2024-06-19 07:58:52 -0700

    Time: 29 ms (21.47%), Space: 12 MB (75.43%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,47 +1,33 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # binary search on the lists
-        # binary search on the list with our number
-        # left, right = 0, len(matrix) - 1
-        # leftNum, rightNum = matrix[left][0], matrix[right][-1]
-        # mid = (left + right) // 2
-        # minNum = matrix[mid][0]
-        # while left <= right
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        LRow, RRow = 0, len(matrix)-1
+        row = -1
         
-        # after loop we will have the index of the array that the target is in
-        # binary search the array for target
-        # leftNum, rightNum = array[left], array[right]
-        # if array[mid] == target: return True
-        
-        # return False
-        
-        top, bot = 0, len(matrix) - 1
-        
-        while top <= bot:
-            row = (top + bot) // 2
-            
-            if target > matrix[row][-1]:
-                top = row + 1
-            elif target < matrix[row][0]:
-                bot = row - 1
-            else:
-                break
-        
-        if not (top <= bot):
-            return False
-        
-        left, right = 0, len(matrix[0]) - 1
-        row = (top + bot) // 2
-        
-        while left <= right:
-            mid = (left + right) // 2
-            midNum = matrix[row][mid]
-            
-            if target > midNum:
-                left = mid + 1
-            elif target < midNum:
-                right = mid - 1
+        while LRow <= RRow:
+            midRow = (LRow + RRow) / 2
+            if matrix[midRow][0] < target:
+                LRow = midRow + 1
+                row = midRow
+            elif matrix[midRow][0] > target:
+                RRow = midRow - 1
             else:
                 return True
             
+        LCol, RCol = 0, len(matrix[0]) - 1
+        midCol = -1
+        
+        while LCol <= RCol:
+            midCol = (LCol + RCol) / 2
+            if matrix[row][midCol] < target:
+                LCol = midCol + 1
+            elif matrix[row][midCol] > target:
+                RCol = midCol - 1
+            else:
+                return True
+        
         return False
